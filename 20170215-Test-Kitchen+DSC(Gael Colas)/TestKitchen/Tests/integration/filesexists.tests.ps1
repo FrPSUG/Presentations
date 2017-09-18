7c2b4446d817f0b93b4efc8d6bee2a494771eec1
François-Xavier Cat <fxcat@lazywinadmin.com>
2017-09-18 15:22:57 +0200

    Ajout scripts de Gael Colas

---
+++ 20170215-Test-Kitchen+DSC(Gael Colas)/TestKitchen/Tests/integration/filesexists.tests.ps1
@@ -0,0 +1,8 @@
+﻿Describe 'checking File Exists' {
+    it 'Should resolve to C:\WinOps.txt' {
+        { Resolve-Path C:\WinOps.txt -ErrorAction Stop } | should not Throw
+    }
+    it 'Should resolve to C:\gotit.txt' {
+        { Resolve-Path C:\gotit.txt -ErrorAction Stop } | should not Throw
+    }
+}
---
+++ 20170215-Test-Kitchen+DSC(Gael Colas)/TestKitchen/examples/dsc_configuration.ps1
@@ -0,0 +1,41 @@
+﻿Configuration Default {
+    Node 'localhost' {
+        # don't do it this way, this is only an illustration of the concepts
+        Script DoSomething
+            {
+                GetScript = { 
+                    Write-verbose 'Entering Get Script'
+                    return @{ 'IsFilePresent' = (Test-Path 'C:\WinOps.txt') }
+                }
+                
+                TestScript = {
+                    # For Idempotence
+                    if (Test-Path 'C:\WinOps.txt')
+                    {
+                        Write-Verbose -Message ('WinOps File is Present')
+                        return $true
+                    }
+                    Write-Verbose -Message ('WinOps File is Absent')
+                    return $false
+                }
+                
+                SetScript = {
+                    Write-verbose 'entering Set Script'
+                    New-Item -Path 'C:\' -Name 'WinOps.txt' -Value 'DSC + Test-Kitchen Rocks'
+                }
+            }
+            Script gotit
+            {
+                GetScript = { 
+                    return @{ 'IsFilePresent' = (Test-Path 'C:\gotit.txt') }
+                }
+                
+                TestScript = {
+                    if (Test-Path 'C:\gotit.txt') { return $true }
+                    return $false
+                }
+                
+                SetScript = { New-Item -Path 'C:\' -Name 'gotit.txt' -Value 'someContent' -ItemType file }
+            }
+    }
+}
