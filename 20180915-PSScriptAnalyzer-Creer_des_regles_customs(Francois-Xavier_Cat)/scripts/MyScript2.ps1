86f8276db2bf91458b8e753423acabeb85929b4a
François-Xavier Cat <fxcat@lazywinadmin.com>
2018-10-01 15:18:04 -0700

    Add PSScriptAnalyzer Custom rules presentation

---
+++ 20180915-PSScriptAnalyzer-Creer_des_regles_customs(Francois-Xavier_Cat)/scripts/MyScript2.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.SYNOPSIS
+    Test script for PSScriptAnalyzer
+#>
+[CmdletBinding()]
+param($File,$credential)
+Try{
+function QuickInventory{
+param([string[]]$Computers)
+$Computers|% -Proc {
+if(Test-Connection $Comp -Quiet){
+    gwmi `
+    -class win32_ComputerSystem `
+    -Cred $credential
+}
+}
+}
+
+function TestSomething{
+Param()
+#does nothing
+}
+
+
+QuickInventory -Comp (gc $File)
+}catch{throw $_}
---
+++ 20180915-PSScriptAnalyzer-Creer_des_regles_customs(Francois-Xavier_Cat)/vscode/psscriptanalyzerrules.settings.psd1
@@ -0,0 +1,4 @@
+@{
+	CustomRulePath = "c:\mymodules\lwacustomrules\1.0.0\lwacustomrules.ps1"
+	IncludeDefaultRules = $true
+}
