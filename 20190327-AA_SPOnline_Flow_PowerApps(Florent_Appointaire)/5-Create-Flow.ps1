9b44269a8f799d7ce4976b0e0b7bbeb29fd5538a
Flodu31 <florent.appointaire@gmail.com>
2019-03-28 08:53:43 +0100

    Add files via upload

---
+++ 20190327-AA_SPOnline_Flow_PowerApps(Florent_Appointaire)/5-Create-Flow.ps1
@@ -0,0 +1,15 @@
+# Install-Module Microsoft.PowerApps.PowerShell
+# Install-Module -Name Microsoft.PowerApps.Administration.PowerShell
+# https://powerapps.microsoft.com/en-us/blog/gdpr-admin-powershell-cmdlets/
+Add-PowerAppsAccount
+Get-Flow
+
+#Flow code:
+
+{
+    "rgName": "",
+    "adminName": "",
+    "PasswordProfile": "",
+    "dnsLabelPrefix": "",
+    "windowsOSVersion": ""
+}
